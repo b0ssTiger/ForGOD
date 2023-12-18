5ae13a80eec81bf25241c54bd1648c73ee2f759e
gajiju <rladydtn730@naver.com>
2023-12-18 20:06:34 +0900

    set2_GameUI
    저장슬롯구현

---
+++ Assets/Script_Kys/FirstSceenScript/UserSaveDataBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class User_Data //세이브 파일 초기안
+{
+    public string nickname = null;
+    public int level = 1;
+    public int coin = 100;
+}
