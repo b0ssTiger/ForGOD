46025ef3b99c5b2431d10539f2c55290b8cedf4e
colarrot <gunho7935@gmail.com>
2023-12-19 20:40:09 +0900

    melee
    근접공격 추가
    활 공격 추가 예정

--- Assets/Character/ArrowManager.cs
+++ Assets/Character/ArrowManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoneManager : MonoBehaviour
+public class ArrowManager : MonoBehaviour
 {
-    private float speed = 2.5f;
+    private float speed = 5.0f;
    [SerializeField] Transform target;
 
     public float timer = 1f;
 
-    void Update()// オブジェクトを目標点まで移動させる
+    void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         Destroy(gameObject, timer);
