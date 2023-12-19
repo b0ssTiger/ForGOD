c4c19f8a7bfac8128a9327bb1a028619b76f87a3
gajiju <rladydtn730@naver.com>
2023-12-19 20:50:34 +0900

    set4_UIveiw
    Sound추가

--- Assets/Script_Kys/SoundManager.cs
+++ Assets/Script_Kys/SoundManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static SoundManager _instance;
+
+    public AudioSource bgSound;
+    public AudioClip[] bgList;
+
+    private void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BgSoundPlay(AudioClip clip)
     {
-        
+        bgSound.clip = clip;
+        bgSound.loop = true;
+        bgSound.volume = 0.1f;
+        bgSound.Play();
     }
 }
