1eb290ec7c367e3fe95b9f0705cd31598b2b9c3c
taeseongHan <taeseong0505@naver.com>
2023-12-19 20:39:22 +0900

    20231219 add monster
    급접 몬스터 전부 구현

--- Assets/HTS/HTS_Scripts/CloseMonster.cs
+++ Assets/HTS/HTS_Scripts/CloseMonster.cs
@@ -14,22 +14,23 @@ public class CloseMonster : MonoBehaviour
     public float followDistance;
     private Slider hpbar;
     public float hpbardistance;
+    public float knockbackForce = 2f;
 
 
     void Start()
     {
         
         mainCamera = Camera.main; // 메인 카메라에 대한 참조를 가져오는 코드
-        currentHp = maxHp;
-        player = GameObject.Find("Player");
-        hpbar = GetComponentInChildren<Slider>();
-        hpbar.maxValue = maxHp;
-        hpbar.value = maxHp;
+        currentHp = maxHp; //현재체력을 풀피와 같게 초기화 작업
+        player = GameObject.Find("Player"); // 플레이어 위치를 알기 위해서 게임오브젝트 플레이어를 Find함
+        hpbar = GetComponentInChildren<Slider>(); //Slider컴포넌트를 value 지정을 위해 가져옴
+        hpbar.maxValue = maxHp; // 최대value가 풀피와 같도록 초기화
+        hpbar.value = maxHp; // value를 풀피와 같도록 초기화
     }
     void Update()
     {
         MonsterMove();
-        UpdateHealthBarPosition();
+        UpdateHpBarPosition();
 
 
     }
@@ -63,6 +64,11 @@ public class CloseMonster : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             TakeDamage(10);
+            //넉백 쓰는거 잘 모르겠음 넉백방향 구하는것까지 알겠음
+            Vector3 knockbackDirection = (transform.position - collision.transform.position).normalized;
+            // 넉백을 적용하기 전에 현재 힘 초기화, 넉백 방향에 힘을 가함 라는데 흠...
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
         }
     }
 
@@ -74,26 +80,27 @@ public class CloseMonster : MonoBehaviour
         {
             Move(player.transform);
         }
-
+        
         Vector3 viewportPos = mainCamera.WorldToViewportPoint(player.transform.position);
-       
+
+
         if (viewportPos.x > 0.5f)
         {
-            
+
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
         else
         {
-           
+
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
 
-    void UpdateHealthBarPosition()
+    void UpdateHpBarPosition()
     {
         // 몬스터의 현재 위치를 기준으로 체력바의 위치를 조절
-        Vector3 healthBarPosition = transform.position + new Vector3(0f, hpbardistance, 0f); // 원하는 위치로 조절
-        hpbar.transform.position = Camera.main.WorldToScreenPoint(healthBarPosition);
+        Vector3 hpBarPosition = transform.position + new Vector3(0f, hpbardistance, 0f); // 원하는 위치로 조절
+        hpbar.transform.position = Camera.main.WorldToScreenPoint(hpBarPosition);
     }
 
 
