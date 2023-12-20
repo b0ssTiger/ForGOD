9070750d5c5fcdfab886fdef4673a3d5efb73e25
Ssoony-93 <tnsrn93@gmail.com>
2023-12-20 20:57:58 +0900

    Inventory5
    GODGPT

--- Assets/Scrips/InventoryScripts/UImanager.cs
+++ Assets/Scrips/InventoryScripts/UImanager.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class UImanager : MonoBehaviour
 {
     // 인벤토리를 열었을 때, 아이템 슬롯에 아이템 정보들이 들어가야함.
     public ItemSlot[] itemslots;
-                       
+
     public void SetInventory()
     {
+        int maxItems = DataManager.instance.gameData.myItems.Length;
 
-        //아이템 정보를 넣음.
-        for (int i = 0; i < DataManager.instance.gameData.myItems.Length; i++)
+        // 아이템 정보를 넣음.
+        for (int i = 0; i < itemslots.Length; i++)
         {
-            itemslots[i].Init(DataManager.instance.gameData.myItems[i]);
-
-            //슬롯 보다 myItems가 많을 경우.
-            //방법1 보유 최대수를 한정함.
-            //방법2 슬롯보다 보유 아이템이 많아지면 슬롯을 더 생성한다.
+            if (i < maxItems)
+            {
+                itemslots[i].Init(DataManager.instance.gameData.myItems[i]);
+            }
 
         }
+    }
+
+    private void CreateNewSlotAndInitItem(Item newItem)
+    {
+        // 새로운 슬롯을 생성하고 아이템을 초기화하여 추가합니다.
+        // 여기서 새로운 슬롯을 생성하는 코드를 작성해야 합니다.
+        // 예를 들어, UI에 새로운 슬롯을 추가하는 방법이 있을 수 있습니다.
+        // 새로운 슬롯을 추가하는 코드를 작성해주세요.
+        // 예시:
+        // ItemSlot newSlot = Instantiate(itemSlotPrefab, parentTransform);
+        // newSlot.Init(newItem);
     }
 }
