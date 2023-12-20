dc40409b75fd872891b47315fafda92f54fc9fc6
gajiju <rladydtn730@naver.com>
2023-12-20 15:18:44 +0900

    setlast_version
    라스트

--- Assets/Character/UserData.cs
+++ Assets/Character/UserData.cs
@@ -6,18 +6,16 @@ using UnityEngine.UI;
 
 public class UserData : MonoBehaviour
 {
-    public string UserName = "cola";
-    public float Maxhp = 100f;
-    public float Curhp = 100f;
-    public int atk = 10;
+
+    public UserStat Player_Stat = new UserStat();
 
     public Animator animator;    
     public Image Hpimage;
 
     public void Start()
     {
-        
-        Maxhp = Curhp;
+
+        Player_Stat.Maxhp = Player_Stat.Curhp;
         
     }
 
@@ -33,19 +31,19 @@ public class UserData : MonoBehaviour
 
     public void Updatehp()
     {
-        float fillAmount = Curhp / Maxhp;
+        float fillAmount = Player_Stat.Curhp / Player_Stat.Maxhp;
         Hpimage.fillAmount = fillAmount; 
     }
 
    
     public void TakeDamage(float damage)
     {
-        
-        Curhp -= damage;
-        Curhp = Mathf.Clamp(Curhp, 0f, Maxhp);
+
+        Player_Stat.Curhp -= damage;
+        Player_Stat.Curhp = Mathf.Clamp(Player_Stat.Curhp, 0f, Player_Stat.Maxhp);
         Updatehp();
         
-        if (Curhp <= 0f) 
+        if (Player_Stat.Curhp <= 0f) 
         {
             UserDie();
         }
@@ -56,6 +54,12 @@ public class UserData : MonoBehaviour
         animator.SetTrigger("Dead");
         // 플레이어 사망 이후 UI 로드 (사망정보창, 부활)
     }
+}
 
-
+public class UserStat
+{
+    public string UserName = "cola";
+    public float Maxhp = 100f;
+    public float Curhp = 100f;
+    public int atk = 10;
 }
