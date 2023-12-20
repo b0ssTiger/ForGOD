71bbf2852f633483847415f5ec75b869b003e743
Ssoony-93 <tnsrn93@gmail.com>
2023-12-20 10:14:37 +0900

    Inventory2
    isEquiped

---
+++ Assets/Scrips/InventoryScripts/PopupEquip.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupEquip : MonoBehaviour
+{
+    public TMP_Text infoText;
+    public Button confirmBtn;
+
+    public void PopupSetting(Items data)
+    {
+        if(data.isEquiped)
+        {
+            infoText.text = "장착을 해제하시겠습니까?";
+            confirmBtn.onClick.RemoveAllListeners();
+            confirmBtn.onClick.AddListener(() => { data.isEquiped = false; });
+
+        }
+        else
+        {
+            infoText.text = "장착 하시겠습니까?";
+            confirmBtn.onClick.RemoveAllListeners();
+            confirmBtn.onClick.AddListener(() => { data.isEquiped = true; });
+        }
+    }
+}
